cde13e8e2a6dda05b7946f64cb8030b70fbb08be
SirTopHat4000 <rileymckeon@seattleacademy.org>
2026-04-23 16:25:26 +0000

    finished p3

--- race.py
+++ race.py
@@ -2,33 +2,33 @@ from utils import *
 
 # Section 1 - Variables
 # TODO - add starting values for all the variables
-x1 = -100
-y1 = 150
-x2 = -100
-y2 = 0
-x3 = -100
-y3 = -100
-x4 = -100
-y4 = 
+x1 = -200
+y1 = 200
+x2 = -200
+y2 = 100
+x3 = -200
+y3 = -50
+x4 = -200
+y4 =  -200
 
 
 # Section 2 - Setup
 # # TODO - use your own background, and set your four turtles to images of your choice
 set_background("brickwall")
 t1 = create_sprite("kirmit with bottle",x1,y1)
-t2 = create_sprite("t",x2,y2)
-t3 = create_sprite("turtle2",x3,y3)
-t4 = create_sprite("turtle2",x4,y4)
+t2 = create_sprite("applecore",x2,y2)
+t3 = create_sprite("mario",x3,y3)
+t4 = create_sprite("turtle3",x4,y4)
 
 
 # # Section 3 - Racin
 # # TODO - set how much each variable changes by and increase the number of repeats to at least 30
 # # TODO - explain here which sprites are faster or slower
 for i in range(3):
-    x1 += 200
-    x2 +=
-    x3 +=
-    x4 +=
+    x1 += 87
+    x2 += 90
+    x3 += 100
+    x4 += random.randint(100,120)
 
     t1.goto(x1, y1)
     t2.goto(x2, y2)
@@ -39,14 +39,14 @@ for i in range(3):
     time.sleep(0.1)
 
 
-# # Section 4 - Winner
-# # TODO - complete the elif for player 2 winning
-# # TODO - write another elif for player 3 and player 4
-# s5 = create_sprite("alien",-200,-200)
-# if x1 >= x2 and x1 >= x3 and x1 >= x4:
-#     s5.write("Player 1 wins!")
-# elif
-#     s5.write("player 2 wins!") 
+# # # Section 4 - Winner
+# # # TODO - complete the elif for player 2 winning
+# # # TODO - write another elif for player 3 and player 4
+s5 = create_sprite("alien",-200,-200)
+if x1 >= x2 and x1 >= x3 and x1 >= x4:
+    s5.write("Player 1 wins!")
+else:
+    s5.write("player 2 wins!") 
 
 
 turtle.exitonclick()
